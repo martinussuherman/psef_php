83a3e93540e9ef6098980423b565719ff35617a1
Martinus Suherman <martinus.suherman@gmail.com>
2021-05-05 15:58:16 +0700

    [WIP] Refactor permohonan selesai (User view)
    - Reformat code

--- modules/pendaftaran-psef/user/selesai_user.php
+++ modules/pendaftaran-psef/user/selesai_user.php
@@ -1,147 +1,148 @@
 <div class="page-breadcrumb">
-    <div class="row">
-        <div class="col-5 align-self-center">
-            <h4 class="page-title" id="page-title">Permohonan (Selesai)</h4>
-            <div class="d-flex align-items-center">
-                <nav aria-label="breadcrumb">
-                    <ol class="breadcrumb" id="list-breadcrumb">
-                        <li class="breadcrumb-item"><a href="javascript:void(0)">Home</a></li>
-                        <li class="breadcrumb-item"><a href="javascript:void(0)">Permohonan (Selesai)</a></li>
-                    </ol>
-                </nav>
-            </div>
-        </div>
-        <div class="col-7 align-self-center">
-            <div class="d-flex no-block justify-content-end align-items-center" id="refresh-page">
-                <button onclick="routing('selesai_user')" type="button" class="btn waves-effect waves-light btn-rounded btn-primary"><i class="fas fa-redo"></i> Segarkan Halaman</button>
-            </div>
-        </div>
-    </div>
+   <div class="row">
+      <div class="col-5 align-self-center">
+         <h4 class="page-title" id="page-title">Permohonan (Selesai)</h4>
+         <div class="d-flex align-items-center">
+            <nav aria-label="breadcrumb">
+               <ol class="breadcrumb" id="list-breadcrumb">
+                  <li class="breadcrumb-item"><a href="javascript:void(0)">Home</a></li>
+                  <li class="breadcrumb-item"><a href="javascript:void(0)">Permohonan (Selesai)</a></li>
+               </ol>
+            </nav>
+         </div>
+      </div>
+      <div class="col-7 align-self-center">
+         <div class="d-flex no-block justify-content-end align-items-center" id="refresh-page">
+            <button onclick="routing('selesai_user')" type="button" class="btn waves-effect waves-light btn-rounded btn-primary"><i class="fas fa-redo"></i> Segarkan Halaman</button>
+         </div>
+      </div>
+   </div>
 </div>
 <div class="container-fluid">
-    <div class="row">
-        <div class="col-12">
-            <div class="card">
-                <div class="card-body" id="load-data">
-                    <div class="row">
-                        <div class="col-5 align-self-center">
-                            <h4 class="page-title">Data Permohonan (Selesai)</h4>
-                        </div>
-                    </div><br>
-                    <div class="table-responsive" id="table-data-here">
-                        <table id="zero_config" class="table table-striped table-bordered" style="width:100%">
-                            <thead>
-                                <tr>
-                                    <th>Nomor Permohonan</th>
-                                    <th>Domain</th>
-                                    <th>Nomor STRA</th>
-                                    <th>Kedaluwarsa STRA</th>
-                                    <th>Status</th>
-                                    <th>Tindakan</th>
-                                </tr>
-                            </thead>
-                        </table>
-                    </div>
-                </div>
+   <div class="row">
+      <div class="col-12">
+         <div class="card">
+            <div class="card-body" id="load-data">
+               <div class="row">
+                  <div class="col-5 align-self-center">
+                     <h4 class="page-title">Data Permohonan (Selesai)</h4>
+                  </div>
+               </div><br>
+               <div class="table-responsive" id="table-data-here">
+                  <table id="zero_config" class="table table-striped table-bordered" style="width:100%">
+                     <thead>
+                        <tr>
+                           <th>Nomor Permohonan</th>
+                           <th>Domain</th>
+                           <th>Nomor STRA</th>
+                           <th>Kedaluwarsa STRA</th>
+                           <th>Status</th>
+                           <th>Tindakan</th>
+                        </tr>
+                     </thead>
+                  </table>
+               </div>
             </div>
-        </div>
-    </div>
+         </div>
+      </div>
+   </div>
 </div>
 
-<?php include ('../template/view_permohonan.html'); ?>
-<?php include ('view_permohonan_script.html'); ?>
+<?php include('../template/view_permohonan.html'); ?>
+<?php include('view_permohonan_script.html'); ?>
 
 <script>
-    var accesstoken = <?php echo json_encode($_COOKIE['accesstoken']); ?>;
-    var arr_detail_add = [];
-    var arr_detail_add_x = [];
-    var id_detail_add = 0;
-    var cc;
-    $(document).ready(function() {
-        $('#zero_config').on('xhr.dt', function ( e, settings, json, xhr ) {
-            json.data = json.rows;
-            json.recordsTotal = json.recordsFiltered = json.total;
-        }).DataTable({
-            "processing": true,
-            "columnDefs": [ {
-                "targets": [4],
-                "orderable": false,
-            } ],
-            "serverSide": true,
-            "scrollY": '100vh',
-            "scrollX": true,
-            "ajax": {
+   var accesstoken = <?php echo json_encode($_COOKIE['accesstoken']); ?>;
+   var arr_detail_add = [];
+   var arr_detail_add_x = [];
+   var id_detail_add = 0;
+   var cc;
+
+   $(document).ready(function() {
+      $('#zero_config').on('xhr.dt', function(e, settings, json, xhr) {
+         json.data = json.rows;
+         json.recordsTotal = json.recordsFiltered = json.total;
+      }).DataTable({
+         "processing": true,
+         "columnDefs": [{
+            "targets": [4],
+            "orderable": false,
+         }],
+         "serverSide": true,
+         "scrollY": '100vh',
+         "scrollX": true,
+         "ajax": {
             "url": url_api_php,
             "type": "POST",
-            "dataSrc": function ( json ) {
-                var data=[];
-                for ( var i=0, ien=json.data.length; i<ien ; i++ ) {
-                var datax = [];
+            "dataSrc": function(json) {
+               var data = [];
 
-                let permohonan = json.data[i];
-                let data_straExpiry = moment(permohonan.straExpiry).format("YYYY-MM-DD");
-                let actions = `
+               for (var i = 0, ien = json.data.length; i < ien; i++) {
+                  var datax = [];
+
+                  let permohonan = json.data[i];
+                  let data_straExpiry = moment(permohonan.straExpiry).format("YYYY-MM-DD");
+                  let actions = `
                   <td>
                      <button onclick="viewPermohonan(${permohonan.id}, false)" type="button" class="btn btn-xs btn-block waves-effect waves-light btn-info">
                         Lihat Detail Data
                      </button>
                   </td>`;
 
-                datax.push(permohonan.permohonanNumber);
-                datax.push(permohonan.domain);
-                datax.push(permohonan.straNumber);
-                datax.push(data_straExpiry);
-                datax.push(permohonan.pemohonStatusName);
-                datax.push(actions);
-                data.push(datax);
-                }
-                return JSON.parse(JSON.stringify(data));
+                  datax.push(permohonan.permohonanNumber);
+                  datax.push(permohonan.domain);
+                  datax.push(permohonan.straNumber);
+                  datax.push(data_straExpiry);
+                  datax.push(permohonan.pemohonStatusName);
+                  datax.push(actions);
+                  data.push(datax);
+               }
 
+               return JSON.parse(JSON.stringify(data));
             },
 
-            "data": function ( d ) {
-                var order_name = []
+            "data": function(d) {
+               var order_name = []
 
-                order_name.push('permohonanNumber');
-                order_name.push('domain');
-                order_name.push('straNumber');
-                order_name.push('straExpiry');
-                order_name.push('statusName');
-                order_name.push('id');
+               order_name.push('permohonanNumber');
+               order_name.push('domain');
+               order_name.push('straNumber');
+               order_name.push('straExpiry');
+               order_name.push('statusName');
+               order_name.push('id');
 
-                var data={};
+               var data = {};
 
-                data.fpage = (parseInt(d.start)+parseInt(d.length))/parseInt(d.length);
-                data.frows = d.length;
-                data.fsearch = d.search['value'];
-                data.forder = 'lastUpdate';
-                data.fsort = 'desc';
-                data.fmodul = 'PermohonanCurrentUser/Selesai';
-                data.flsearch = 'permohonanNumber,domain,straNumber';
-                data.ftots = 3;
-                return data;
+               data.fpage = (parseInt(d.start) + parseInt(d.length)) / parseInt(d.length);
+               data.frows = d.length;
+               data.fsearch = d.search['value'];
+               data.forder = 'lastUpdate';
+               data.fsort = 'desc';
+               data.fmodul = 'PermohonanCurrentUser/Selesai';
+               data.flsearch = 'permohonanNumber,domain,straNumber';
+               data.ftots = 3;
+               return data;
             }
-            }
-        });
-    });
+         }
+      });
+   });
 
-    function viewRouting() {
+   function viewRouting() {
       routing('selesai_user');
-    }
+   }
 
-    $('.modal-close-conf').click(function() {
-        close_modals();
-    })
+   $('.modal-close-conf').click(function() {
+      close_modals();
+   })
 
-    function close_modals() {
-        $('.modal-close').click();
-        $('#name').val('')
-        $('#siaNumber').val('')
-        $('#apotekerName').val('')
-        $('#straNumber').val('')
-        $('#sipaNumber').val('')
-        $('#address').val('')
-        $('select[name="provinsiId"]').val(null).trigger("change");
-    }
-
+   function close_modals() {
+      $('.modal-close').click();
+      $('#name').val('')
+      $('#siaNumber').val('')
+      $('#apotekerName').val('')
+      $('#straNumber').val('')
+      $('#sipaNumber').val('')
+      $('#address').val('')
+      $('select[name="provinsiId"]').val(null).trigger("change");
+   }
 </script>
