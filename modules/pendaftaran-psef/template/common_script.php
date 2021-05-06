3a0e368dbb2244d2a1bb5c52735cacabd63445a3
Martinus Suherman <martinus.suherman@gmail.com>
2021-05-06 19:19:04 +0700

    Update template for permohonan common script
    - Add function progressPermohonanFromStatus

--- modules/pendaftaran-psef/template/common_script.php
+++ modules/pendaftaran-psef/template/common_script.php
@@ -28,4 +28,30 @@
     dataPermohonan.name_imbUrl = fileNameFromUrl(dataPermohonan.imbUrl);
     dataPermohonan.name_pembayaranPnbpUrl = fileNameFromUrl(dataPermohonan.pembayaranPnbpUrl);
   }
+
+  function progressPermohonanFromStatus(statusId) {
+    switch (statusId) {
+      case 4:
+        return 1;
+      case 6:
+        return 2;
+      case 8:
+        return 3;
+      case 5:
+      case 10:
+        return 4;
+      case 7:
+      case 12:
+        return 5;
+      case 9:
+        return 6;
+      case 11:
+        return 7;
+      case 13:
+        return 9;
+    }
+
+    return statusId;
+  }
+
 </script>
