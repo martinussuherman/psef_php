b239d42f0729e18017a110119485646d98044b8f
Martinus Suherman <martinus.suherman@gmail.com>
2021-05-06 17:46:05 +0700

    Create template for dokumen view fragment

---
+++ modules/pendaftaran-psef/template/view_dokumen.php
@@ -0,0 +1,81 @@
+<!-- Dokumen Penunjang -->
+<div class="row">
+  <div class="col-md-4">
+    <div class="form-group">
+      <label class="control-label">Salinan STRA</label>
+      <div class="border p-10" style="background-color: #e9ecef;padding: .375rem .75rem;">
+        <a href="https://psef.kemkes.go.id{{data_permohonan.straUrl}}">{{data_permohonan.name_straUrl}}</a>
+      </div>
+    </div>
+  </div>
+  <div class="col-md-4">
+    <div class="form-group">
+      <label class="control-label">Surat Permohonan</label>
+      <div class="border p-10" style="background-color: #e9ecef;padding: .375rem .75rem;">
+        <a href="https://psef.kemkes.go.id{{data_permohonan.suratPermohonanUrl}}">{{data_permohonan.name_suratPermohonanUrl}}</a>
+      </div>
+    </div>
+  </div>
+  <div class="col-md-4">
+    <div class="form-group">
+      <label class="control-label">Dokumen Proses Bisnis</label>
+      <div class="border p-10" style="background-color: #e9ecef;padding: .375rem .75rem;">
+        <a href="https://psef.kemkes.go.id{{data_permohonan.prosesBisnisUrl}}">{{data_permohonan.name_prosesBisnisUrl}}</a>
+      </div>
+    </div>
+  </div>
+</div>
+
+<div class="row">
+  <div class="col-md-4">
+    <div class="form-group">
+      <label class="control-label">Dokumen Application Programmer Interface Sistem PSEF</label>
+      <div class="border p-10" style="background-color: #e9ecef;padding: .375rem .75rem;">
+        <a href="https://psef.kemkes.go.id{{data_permohonan.dokumenApiUrl}}">{{data_permohonan.name_dokumenApiUrl}}</a>
+      </div>
+    </div>
+  </div>
+  <div class="col-md-4">
+    <div class="form-group">
+      <label class="control-label">Dokumen PSE Kominfo</label>
+      <div class="border p-10" style="background-color: #e9ecef;padding: .375rem .75rem;">
+        <a href="https://psef.kemkes.go.id{{data_permohonan.dokumenPseUrl}}">{{data_permohonan.name_dokumenPseUrl}}</a>
+      </div>
+    </div>
+  </div>
+  <div class="col-md-4">
+    <div class="form-group">
+      <label class="control-label">SPPL</label>
+      <div class="border p-10" style="background-color: #e9ecef;padding: .375rem .75rem;">
+        <a href="https://psef.kemkes.go.id{{data_permohonan.spplUrl}}">{{data_permohonan.name_spplUrl}}</a>
+      </div>
+    </div>
+  </div>
+</div>
+
+<div class="row">
+  <div class="col-md-4">
+    <div class="form-group">
+      <label class="control-label">Izin Lokasi</label>
+      <div class="border p-10" style="background-color: #e9ecef;padding: .375rem .75rem;">
+        <a href="https://psef.kemkes.go.id{{data_permohonan.izinLokasiUrl}}">{{data_permohonan.name_izinLokasiUrl}}</a>
+      </div>
+    </div>
+  </div>
+  <div class="col-md-4">
+    <div class="form-group">
+      <label class="control-label">IMB</label>
+      <div class="border p-10" style="background-color: #e9ecef;padding: .375rem .75rem;">
+        <a href="https://psef.kemkes.go.id{{data_permohonan.imbUrl}}">{{data_permohonan.name_imbUrl}}</a>
+      </div>
+    </div>
+  </div>
+  <div class="col-md-4">
+    <div class="form-group">
+      <label class="control-label">Pembayaran PNBP</label>
+      <div class="border p-10" style="background-color: #e9ecef;padding: .375rem .75rem;">
+        <a href="https://psef.kemkes.go.id{{data_permohonan.pembayaranPnbpUrl}}">{{data_permohonan.name_pembayaranPnbpUrl}}</a>
+      </div>
+    </div>
+  </div>
+</div>
