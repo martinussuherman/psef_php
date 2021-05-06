d5fd9a11c45cf15fe9b4ca46903277d26fe0532f
Martinus Suherman <martinus.suherman@gmail.com>
2021-05-06 15:01:43 +0700

    [WIP] Refactor permohonan rumusan (User view)
    - Use permohonan template

--- modules/pendaftaran-psef/user/rumusan_user.php
+++ modules/pendaftaran-psef/user/rumusan_user.php
@@ -1,57 +1,9 @@
-<div class="page-breadcrumb">
-   <div class="row">
-      <div class="col-5 align-self-center">
-         <h4 class="page-title" id="page-title">Permohonan (Rumusan)</h4>
-         <div class="d-flex align-items-center">
-            <nav aria-label="breadcrumb">
-               <ol class="breadcrumb" id="list-breadcrumb">
-                  <li class="breadcrumb-item"><a href="javascript:void(0)">Home</a></li>
-                  <li class="breadcrumb-item"><a href="javascript:void(0)">Permohonan (Rumusan)</a></li>
-               </ol>
-            </nav>
-         </div>
-      </div>
-      <div class="col-7 align-self-center">
-         <div class="d-flex no-block justify-content-end align-items-center" id="refresh-page">
-            <button onclick="routing('rumusan_user')" type="button" class="btn waves-effect waves-light btn-rounded btn-primary"><i class="fas fa-redo"></i> Segarkan Halaman</button>
-         </div>
-      </div>
-   </div>
-</div>
-<div class="container-fluid">
-   <div class="row">
-      <div class="col-12">
-         <div class="card">
-            <div class="card-body" id="load-data">
-               <div class="row">
-                  <div class="col-5 align-self-center">
-                     <h4 class="page-title">Data Permohonan (Rumusan)</h4>
-                  </div>
-                  <div class="col-7 align-self-center">
-                     <div class="d-flex no-block justify-content-end align-items-center" id="add_data">
-                        <button onclick="add_data()" type="button" class="btn waves-effect waves-light btn-info btn-add-data">Tambah Data Permohonan</button>
-                     </div>
-                  </div>
-               </div><br>
-               <div class="table-responsive" id="table-data-here">
-                  <table id="zero_config" class="table table-striped table-bordered" style="width:100%">
-                     <thead>
-                        <tr>
-                           <th>Nomor Permohonan</th>
-                           <th>Domain</th>
-                           <th>Nomor STRA</th>
-                           <th>Kedaluwarsa STRA</th>
-                           <th>Status</th>
-                           <th>Tindakan</th>
-                        </tr>
-                     </thead>
-                  </table>
-               </div>
-            </div>
-         </div>
-      </div>
-   </div>
-</div>
+<?php
+$showAddData = true;
+$pageType = 'rumusan';
+$pageTitle = 'Permohonan (Rumusan)';
+include('template.php');
+?>
 
 <?php include('../template/add_permohonan.html'); ?>
 <?php include('../template/edit_permohonan.html'); ?>
