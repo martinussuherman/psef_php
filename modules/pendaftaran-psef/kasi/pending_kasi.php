a9996e4aa85dcb88dd4e98994a2b98adb98bb84a
Martinus Suherman <martinus.suherman@gmail.com>
2021-05-06 19:16:54 +0700

    [WIP] Refactor permohonan pending (Kasi view)
    - Add function viewRouting

--- modules/pendaftaran-psef/kasi/pending_kasi.php
+++ modules/pendaftaran-psef/kasi/pending_kasi.php
@@ -185,6 +185,10 @@ include('../template/modal_nib.php');
         });
     });
 
+    function viewRouting() {
+      routing('pending_kasi');
+    }
+
     function view_data(id){
         $.ajax({
             url: url_api_x+"Permohonan("+id+")",
