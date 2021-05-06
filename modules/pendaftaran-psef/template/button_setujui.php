c6384da96d915e108ac3c90740c2665494dd9d94
Martinus Suherman <martinus.suherman@gmail.com>
2021-05-06 17:47:46 +0700

    Create template for button setujui view fragment

---
+++ modules/pendaftaran-psef/template/button_setujui.php
@@ -0,0 +1,7 @@
+<button type="button" class="btn btn-success" onclick="process_data('{{data_permohonan.id}}')">
+  Setujui
+</button>
+
+<button type="button" class="btn btn-warning" onclick="reject_data('{{data_permohonan.id}}')">
+  Kembalikan
+</button>
