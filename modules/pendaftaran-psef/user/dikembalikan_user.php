b9da8c736889c98cf3f46dd76e5fc91f539d1330
Martinus Suherman <martinus.suherman@gmail.com>
2021-05-06 15:00:49 +0700

    [WIP] Refactor permohonan dikembalikan (User view)
    - Use permohonan template

--- modules/pendaftaran-psef/user/dikembalikan_user.php
+++ modules/pendaftaran-psef/user/dikembalikan_user.php
@@ -1,52 +1,8 @@
-<div class="page-breadcrumb">
-   <div class="row">
-      <div class="col-5 align-self-center">
-         <h4 class="page-title" id="page-title">Permohonan (Dikembalikan)</h4>
-         <div class="d-flex align-items-center">
-            <nav aria-label="breadcrumb">
-               <ol class="breadcrumb" id="list-breadcrumb">
-                  <li class="breadcrumb-item"><a href="javascript:void(0)">Home</a></li>
-                  <li class="breadcrumb-item"><a href="javascript:void(0)">Permohonan (Dikembalikan)</a></li>
-               </ol>
-            </nav>
-         </div>
-      </div>
-      <div class="col-7 align-self-center">
-         <div class="d-flex no-block justify-content-end align-items-center" id="refresh-page">
-            <button onclick="routing('dikembalikan_user')" type="button" class="btn waves-effect waves-light btn-rounded btn-primary"><i class="fas fa-redo"></i> Segarkan Halaman</button>
-         </div>
-      </div>
-   </div>
-</div>
-<div class="container-fluid">
-   <div class="row">
-      <div class="col-12">
-         <div class="card">
-            <div class="card-body" id="load-data">
-               <div class="row">
-                  <div class="col-5 align-self-center">
-                     <h4 class="page-title">Data Permohonan (Dikembalikan)</h4>
-                  </div>
-               </div><br>
-               <div class="table-responsive" id="table-data-here">
-                  <table id="zero_config" class="table table-striped table-bordered" style="width:100%">
-                     <thead>
-                        <tr>
-                           <th>Nomor Permohonan</th>
-                           <th>Domain</th>
-                           <th>Nomor STRA</th>
-                           <th>Kedaluwarsa STRA</th>
-                           <th>Status</th>
-                           <th>Tindakan</th>
-                        </tr>
-                     </thead>
-                  </table>
-               </div>
-            </div>
-         </div>
-      </div>
-   </div>
-</div>
+<?php
+$pageType = 'dikembalikan';
+$pageTitle = 'Permohonan (Dikembalikan)';
+include('template.php');
+?>
 
 <?php include('../template/edit_permohonan.html'); ?>
 <?php include('../template/view_permohonan.html'); ?>
