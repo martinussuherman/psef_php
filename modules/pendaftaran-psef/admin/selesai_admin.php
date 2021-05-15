0579da0487ade1700c65deafb5bd955f453e7fd6
Martinus Suherman <martinus.suherman@gmail.com>
2021-05-15 19:32:46 +0700

    [WIP] Refactor permohonan selesai (Admin view)
    - Use template for permohonan list

--- modules/pendaftaran-psef/admin/selesai_admin.php
+++ modules/pendaftaran-psef/admin/selesai_admin.php
@@ -1,58 +1,9 @@
-<div class="page-breadcrumb">
-    <div class="row">
-        <div class="col-5 align-self-center">
-            <h4 class="page-title" id="page-title">Permohonan (Selesai)</h4>
-            <div class="d-flex align-items-center">
-                <nav aria-label="breadcrumb">
-                    <ol class="breadcrumb" id="list-breadcrumb">
-                        <li class="breadcrumb-item"><a href="javascript:void(0)">Home</a></li>
-                        <li class="breadcrumb-item"><a href="javascript:void(0)">Permohonan (Selesai)</a></li>
-                    </ol>
-                </nav>
-            </div>
-        </div>
-        <div class="col-7 align-self-center">
-            <div class="d-flex no-block justify-content-end align-items-center" id="refresh-page">
-                <button onclick="routing('selesai_admin')" type="button" class="btn waves-effect waves-light btn-rounded btn-primary"><i class="fas fa-redo"></i> Segarkan Halaman</button>
-            </div>
-        </div>
-    </div>
-</div>
-<div class="container-fluid">
-    <div class="row">
-        <div class="col-12">
-            <div class="card">
-                <div class="card-body" id="load-data">
-                    <div class="row">
-                        <div class="col-5 align-self-center">
-                            <h4 class="page-title">Data Permohonan (Selesai)</h4>
-                        </div>
-                    </div><br>
-                    <div class="table-responsive" id="table-data-here">
-                        <table id="zero_config" class="table table-striped table-bordered" style="width:100%">
-                            <thead>
-                                <tr>
-                                    <th>Nomor Permohonan</th>
-                                    <th>Domain</th>
-                                    <th>Nomor STRA</th>
-                                    <th>Kedaluwarsa STRA</th>
-                                    <th>Status</th>
-                                    <th>Tindakan</th>
-                                </tr>
-                            </thead>
-                        </table>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
-
 <?php
 $isKemkesView = true;
 $showRekamJejak = true;
 
-// $pageTitle = 'Permohonan (Rumusan)';
+$pageTitle = 'Permohonan (Selesai)';
+include('../template/template_kemkes.php');
 include('../template/common_script.php');
 include('../template/view_permohonan.php');
 include('../template/modal_nib.php');
