c5ebfa8f96afae6afbdf1df40fb76227c843bb9d
Martinus Suherman <martinus.suherman@gmail.com>
2021-05-17 16:36:03 +0700

    [WIP] Refactor perizinan (Validator view)
    - Add function viewRouting

--- modules/perizinan/validator/perizinan_validator.php
+++ modules/perizinan/validator/perizinan_validator.php
@@ -108,6 +108,10 @@ include('../template/view_perizinan.php');
         });
     });
 
+    function viewRouting() {
+      routing('perizinan_validator');
+    }
+
     function view_data(id,id_izin){
         $.ajax({
             url: url_api_x+"Permohonan("+id+")",
