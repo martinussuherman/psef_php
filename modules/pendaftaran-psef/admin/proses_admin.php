1c4ba0ba51b8cf11ab66778f7c78e5305278645b
Martinus Suherman <martinus.suherman@gmail.com>
2021-05-28 18:39:08 +0700

    [WIP] Refactor permohonan dalam proses (Admin view)
    - Use function configurePermohonanRequest

--- modules/pendaftaran-psef/admin/proses_admin.php
+++ modules/pendaftaran-psef/admin/proses_admin.php
@@ -46,26 +46,7 @@ include('../template/template_kemkes.php');
             },
 
             "data": function ( d ) {
-                var order_name = []
-
-                order_name.push('permohonanNumber');
-                order_name.push('domain');
-                order_name.push('straNumber');
-                order_name.push('straExpiry');
-                order_name.push('statusName');
-                order_name.push('id');
-
-                var data={};
-
-                data.fpage = (parseInt(d.start)+parseInt(d.length))/parseInt(d.length);
-                data.frows = d.length;
-                data.fsearch = d.search['value'];
-                data.forder = 'lastUpdate';
-                data.fsort = 'desc';
-                data.fmodul = 'Permohonan/Progress';
-                data.flsearch = 'permohonanNumber,domain,straNumber';
-                data.ftots = 3;
-                return data;
+              return configurePermohonanRequest(d, 'Permohonan/Progress');
             }
             }
         });
