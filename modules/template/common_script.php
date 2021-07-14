5d85ba267b6bd9b24d8660f9ef9b6c9c425a31fd
Martinus Suherman <martinus.suherman@gmail.com>
2021-07-14 14:12:38 +0700

    Update template for common script
    - Add function scrollToTop

--- modules/template/common_script.php
+++ modules/template/common_script.php
@@ -654,4 +654,9 @@
         permohonanTandaDaftar(id, '', passphrase, apiUrl, token);
       });
   }
+
+  function scrollToTop() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+  }
 </script>
