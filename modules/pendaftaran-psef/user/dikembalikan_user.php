fb49108439e7939ab904e514639ad02bbb88c399
Martinus Suherman <martinus.suherman@gmail.com>
2021-07-21 15:04:49 +0700

    [WIP] Refactor permohonan dikembalikan (User view)
    - Reformat code

--- modules/pendaftaran-psef/user/dikembalikan_user.php
+++ modules/pendaftaran-psef/user/dikembalikan_user.php
@@ -27,45 +27,45 @@ include('edit_permohonan_script.html');
 ?>
 
 <script>
-   var accesstoken = <?php echo json_encode($_COOKIE['accesstoken']); ?>;
-   var arr_detail_add = [];
-   var arr_detail_add_x = [];
-   var arr_detail_add_klinik = [];
-   var arr_detail_add_klinik_x = [];
-   var arr_detail_add_rs = [];
-   var arr_detail_add_rs_x = [];
-   var id_detail_add = 0;
-   var id_detail_add_klinik = 0;
-   var id_detail_add_rs = 0;
-   var cc;
+  var accesstoken = <?php echo json_encode($_COOKIE['accesstoken']); ?>;
+  var arr_detail_add = [];
+  var arr_detail_add_x = [];
+  var arr_detail_add_klinik = [];
+  var arr_detail_add_klinik_x = [];
+  var arr_detail_add_rs = [];
+  var arr_detail_add_rs_x = [];
+  var id_detail_add = 0;
+  var id_detail_add_klinik = 0;
+  var id_detail_add_rs = 0;
+  var cc;
 
-   $(document).ready(function() {
-      $('#zero_config').on('xhr.dt', function(e, settings, json, xhr) {
-         json.data = json.rows;
-         json.recordsTotal = json.recordsFiltered = json.total;
-      }).DataTable({
-         "processing": true,
-         "columnDefs": [{
-            "targets": [4],
-            "orderable": false,
-         }],
-         "serverSide": true,
-         "scrollY": '100vh',
-         "scrollX": true,
-         "ajax": {
-            "url": url_api_php,
-            "type": "POST",
-            "dataSrc": function(json) {
-               return permohonanFromJson(json, false);
-            },
-            "data": function(d) {
-               return permohonanAjaxRequest(d, 'PermohonanCurrentUser/Dikembalikan');
-            }
-         }
-      });
-   });
+  $(document).ready(function() {
+    $('#zero_config').on('xhr.dt', function(e, settings, json, xhr) {
+      json.data = json.rows;
+      json.recordsTotal = json.recordsFiltered = json.total;
+    }).DataTable({
+      "processing": true,
+      "columnDefs": [{
+        "targets": [4],
+        "orderable": false,
+      }],
+      "serverSide": true,
+      "scrollY": '100vh',
+      "scrollX": true,
+      "ajax": {
+        "url": url_api_php,
+        "type": "POST",
+        "dataSrc": function(json) {
+          return permohonanFromJson(json, false);
+        },
+        "data": function(d) {
+          return permohonanAjaxRequest(d, 'PermohonanCurrentUser/Dikembalikan');
+        }
+      }
+    });
+  });
 
-   function viewRouting() {
-      routing('dikembalikan_user');
-   }
+  function viewRouting() {
+    routing('dikembalikan_user');
+  }
 </script>
