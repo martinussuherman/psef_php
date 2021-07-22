d9f4753a74735bac3538c58a00cedcc3a099bc38
Martinus Suherman <martinus.suherman@gmail.com>
2021-07-22 18:45:32 +0700

    Update edit permohonan template
    - Use template for contoh surat

--- modules/pendaftaran-psef/template/edit_permohonan.php
+++ modules/pendaftaran-psef/template/edit_permohonan.php
@@ -70,20 +70,9 @@
               </div>
           </div>
         </div>
-        <div class="form-group">
-            <label><b>Contoh Surat</b></label>
-            <div class="border p-10" style="background-color: #e9ecef;padding: .375rem .75rem;">
-                <a href="https://psef.kemkes.go.id/assets/doc/Contoh%20Surat%20Permohonan%20Tanda%20Daftar%20PSEF.docx">Contoh Surat Permohonan</a>
-            </div>
-            <br>
-            <div class="border p-10" style="background-color: #e9ecef;padding: .375rem .75rem;">
-                <a href="https://psef.kemkes.go.id/assets/doc/Contoh%20Dokumen%20PSE%20Kominfo.docx">Contoh Dokumen PSE Kominfo</a>
-            </div>
-            <br>
-            <div class="border p-10" style="background-color: #e9ecef;padding: .375rem .75rem;">
-                <a href="https://psef.kemkes.go.id/assets/doc/Contoh%20Surat%20Pernyataan%20Komitmen%20Bekerjasama%20dengan%20Apotek.docx">Contoh Surat Pernyataan Komitmen bekerjasama dengan Apotek</a>
-            </div>
-        </div>
+
+<?php include('permohonan_contoh_surat.php'); ?>
+
         <div class="form-group">
             <label>Salinan STRA</label>
             <div class="border p-10" style="background-color: #e9ecef;padding: .375rem .75rem;">
