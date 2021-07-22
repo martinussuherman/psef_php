3710c34f3ff540e0c187a6957664ad291a3e8562
Martinus Suherman <martinus.suherman@gmail.com>
2021-07-22 18:45:05 +0700

    Update add permohonan template
    - Use template for contoh surat

--- modules/pendaftaran-psef/template/add_permohonan.php
+++ modules/pendaftaran-psef/template/add_permohonan.php
@@ -70,21 +70,9 @@
               </div>
           </div>
         </div>
-        <div class="form-group">
-          <label>
-            <b>Contoh Surat</b>
-          </label>
 
-          <div class="border px-3 py-1 mb-2" style="background-color: #e9ecef;">
-            <a href="https://psef.kemkes.go.id/assets/doc/Contoh%20Surat%20Permohonan%20Tanda%20Daftar%20PSEF.docx">Contoh Surat Permohonan</a>
-          </div>
-          <div class="border px-3 py-1 mb-2" style="background-color: #e9ecef;">
-            <a href="https://psef.kemkes.go.id/assets/doc/Contoh%20Dokumen%20PSE%20Kominfo.docx">Contoh Dokumen PSE Kominfo</a>
-          </div>
-          <div class="border px-3 py-1 mb-2" style="background-color: #e9ecef;">
-            <a href="https://psef.kemkes.go.id/assets/doc/Contoh%20Surat%20Pernyataan%20Komitmen%20Bekerjasama%20dengan%20Apotek.docx">Contoh Surat Pernyataan Komitmen bekerjasama dengan Apotek</a>
-          </div>
-        </div>
+<?php include('permohonan_contoh_surat.php'); ?>
+
         <div class="form-group">
             <label>Salinan STRA</label>
             <input type="file" class="form-control" id="straUrl" required>
