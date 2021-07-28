4f628ad940d90152182aed259c430ec05ccd2d5c
Martinus Suherman <martinus.suherman@gmail.com>
2021-07-28 14:01:19 +0700

    Update edit permohonan template
    - Remove required from input Keaslian Dokumen

--- modules/pendaftaran-psef/template/edit_permohonan.php
+++ modules/pendaftaran-psef/template/edit_permohonan.php
@@ -141,7 +141,7 @@
           <div class="border p-10" style="background-color: #e9ecef;padding: .375rem .75rem;">
               <a href="https://psef.kemkes.go.id{{pernyataanKeaslianDokumenUrl}}" id="close-pernyataanKeaslianDokumenUrl">{{name_pernyataanKeaslianDokumenUrl}}</a>
           </div>
-          <input type="file" class="form-control" id="pernyataanKeaslianDokumenUrl" required>
+          <input type="file" class="form-control" id="pernyataanKeaslianDokumenUrl">
           <small class="form-text text-muted">*Berkas yang anda upload wajib PDF & size file maksimal 5 MB</small>
           <input type="hidden" name="pernyataanKeaslianDokumenUrl" id="v-pernyataanKeaslianDokumenUrl">
         </div>
