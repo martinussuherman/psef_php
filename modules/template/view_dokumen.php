b73d81504c000f2d13b0a5a1515b91e014924ae6
Martinus Suherman <martinus.suherman@gmail.com>
2021-07-30 10:25:10 +0700

    Update template for dokumen view fragment
    - Open document link on new tab/window

--- modules/template/view_dokumen.php
+++ modules/template/view_dokumen.php
@@ -4,7 +4,7 @@
     <div class="form-group">
       <label class="control-label">Salinan STRA</label>
       <div class="border p-10" style="background-color: #e9ecef;padding: .375rem .75rem;">
-        <a href="https://psef.kemkes.go.id{{data_permohonan.straUrl}}">{{data_permohonan.name_straUrl}}</a>
+        <a href="https://psef.kemkes.go.id{{data_permohonan.straUrl}}" target="_blank">{{data_permohonan.name_straUrl}}</a>
       </div>
     </div>
   </div>
@@ -12,7 +12,7 @@
     <div class="form-group">
       <label class="control-label">Surat Permohonan</label>
       <div class="border p-10" style="background-color: #e9ecef;padding: .375rem .75rem;">
-        <a href="https://psef.kemkes.go.id{{data_permohonan.suratPermohonanUrl}}">{{data_permohonan.name_suratPermohonanUrl}}</a>
+        <a href="https://psef.kemkes.go.id{{data_permohonan.suratPermohonanUrl}}" target="_blank">{{data_permohonan.name_suratPermohonanUrl}}</a>
       </div>
     </div>
   </div>
@@ -20,7 +20,7 @@
     <div class="form-group">
       <label class="control-label">Dokumen Proses Bisnis</label>
       <div class="border p-10" style="background-color: #e9ecef;padding: .375rem .75rem;">
-        <a href="https://psef.kemkes.go.id{{data_permohonan.prosesBisnisUrl}}">{{data_permohonan.name_prosesBisnisUrl}}</a>
+        <a href="https://psef.kemkes.go.id{{data_permohonan.prosesBisnisUrl}}" target="_blank">{{data_permohonan.name_prosesBisnisUrl}}</a>
       </div>
     </div>
   </div>
@@ -31,7 +31,7 @@
     <div class="form-group">
       <label class="control-label">Dokumen Application Programmer Interface Sistem PSEF</label>
       <div class="border p-10" style="background-color: #e9ecef;padding: .375rem .75rem;">
-        <a href="https://psef.kemkes.go.id{{data_permohonan.dokumenApiUrl}}">{{data_permohonan.name_dokumenApiUrl}}</a>
+        <a href="https://psef.kemkes.go.id{{data_permohonan.dokumenApiUrl}}" target="_blank">{{data_permohonan.name_dokumenApiUrl}}</a>
       </div>
     </div>
   </div>
@@ -39,7 +39,7 @@
     <div class="form-group">
       <label class="control-label">Dokumen PSE Kominfo</label>
       <div class="border p-10" style="background-color: #e9ecef;padding: .375rem .75rem;">
-        <a href="https://psef.kemkes.go.id{{data_permohonan.dokumenPseUrl}}">{{data_permohonan.name_dokumenPseUrl}}</a>
+        <a href="https://psef.kemkes.go.id{{data_permohonan.dokumenPseUrl}}" target="_blank">{{data_permohonan.name_dokumenPseUrl}}</a>
       </div>
     </div>
   </div>
@@ -47,7 +47,7 @@
     <div class="form-group">
       <label class="control-label">Dokumen Izin Usaha Berbentuk IUI/PMSE</label>
       <div class="border p-10" style="background-color: #e9ecef;padding: .375rem .75rem;">
-        <a href="https://psef.kemkes.go.id{{data_permohonan.izinUsahaUrl}}">{{data_permohonan.name_izinUsahaUrl}}</a>
+        <a href="https://psef.kemkes.go.id{{data_permohonan.izinUsahaUrl}}" target="_blank">{{data_permohonan.name_izinUsahaUrl}}</a>
       </div>
     </div>
   </div>
@@ -55,7 +55,7 @@
     <div class="form-group">
       <label class="control-label">SPPL</label>
       <div class="border p-10" style="background-color: #e9ecef;padding: .375rem .75rem;">
-        <a href="https://psef.kemkes.go.id{{data_permohonan.spplUrl}}">{{data_permohonan.name_spplUrl}}</a>
+        <a href="https://psef.kemkes.go.id{{data_permohonan.spplUrl}}" target="_blank">{{data_permohonan.name_spplUrl}}</a>
       </div>
     </div>
   </div> -->
@@ -66,7 +66,7 @@
     <div class="form-group">
       <label class="control-label">Surat Pernyataan Komitmen bekerjasama dengan Apotek</label>
       <div class="border p-10" style="background-color: #e9ecef;padding: .375rem .75rem;">
-        <a href="https://psef.kemkes.go.id{{data_permohonan.komitmenKerjasamaApotekUrl}}">{{data_permohonan.name_komitmenKerjasamaApotekUrl}}</a>
+        <a href="https://psef.kemkes.go.id{{data_permohonan.komitmenKerjasamaApotekUrl}}" target="_blank">{{data_permohonan.name_komitmenKerjasamaApotekUrl}}</a>
       </div>
     </div>
   </div>
@@ -74,7 +74,7 @@
     <div class="form-group">
       <label class="control-label">Surat Pernyataan Keaslian Dokumen</label>
       <div class="border p-10" style="background-color: #e9ecef;padding: .375rem .75rem;">
-        <a href="https://psef.kemkes.go.id{{data_permohonan.pernyataanKeaslianDokumenUrl}}">{{data_permohonan.name_pernyataanKeaslianDokumenUrl}}</a>
+        <a href="https://psef.kemkes.go.id{{data_permohonan.pernyataanKeaslianDokumenUrl}}" target="_blank">{{data_permohonan.name_pernyataanKeaslianDokumenUrl}}</a>
       </div>
     </div>
   </div>
@@ -85,7 +85,7 @@
     <div class="form-group">
       <label class="control-label">Izin Lokasi</label>
       <div class="border p-10" style="background-color: #e9ecef;padding: .375rem .75rem;">
-        <a href="https://psef.kemkes.go.id{{data_permohonan.izinLokasiUrl}}">{{data_permohonan.name_izinLokasiUrl}}</a>
+        <a href="https://psef.kemkes.go.id{{data_permohonan.izinLokasiUrl}}" target="_blank">{{data_permohonan.name_izinLokasiUrl}}</a>
       </div>
     </div>
   </div>
@@ -93,7 +93,7 @@
     <div class="form-group">
       <label class="control-label">IMB</label>
       <div class="border p-10" style="background-color: #e9ecef;padding: .375rem .75rem;">
-        <a href="https://psef.kemkes.go.id{{data_permohonan.imbUrl}}">{{data_permohonan.name_imbUrl}}</a>
+        <a href="https://psef.kemkes.go.id{{data_permohonan.imbUrl}}" target="_blank">{{data_permohonan.name_imbUrl}}</a>
       </div>
     </div>
   </div>
@@ -101,7 +101,7 @@
     <div class="form-group">
       <label class="control-label">Pembayaran PNBP</label>
       <div class="border p-10" style="background-color: #e9ecef;padding: .375rem .75rem;">
-        <a href="https://psef.kemkes.go.id{{data_permohonan.pembayaranPnbpUrl}}">{{data_permohonan.name_pembayaranPnbpUrl}}</a>
+        <a href="https://psef.kemkes.go.id{{data_permohonan.pembayaranPnbpUrl}}" target="_blank">{{data_permohonan.name_pembayaranPnbpUrl}}</a>
       </div>
     </div>
   </div>
