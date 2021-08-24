7e84f4919b586e2d003c7206167d865d9bfdfe49
Martinus Suherman <martinus.suherman@gmail.com>
2021-08-24 19:10:46 +0700

    Delete unused code (button component) - moved to buttonPartial.php

--- modules/pendaftaran-psef/template/button_ajukan.php
+++
@@ -1,3 +0,0 @@
-<button type="button" class="btn btn-success" onclick="ajukan_permohonan('{{data_permohonan.id}}')">
-  Ajukan Permohonan
-</button>
--- modules/pendaftaran-psef/template/button_validasi.php
+++
@@ -1,3 +0,0 @@
-<button type="button" class="btn btn-success" onclick="process_data('{{data_permohonan.id}}')">
-  Proses Data
-</button>
