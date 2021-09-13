2b03f55a4e18954f51dac13299fe838c2f6ed1b5
Martinus Suherman <martinus.suherman@gmail.com>
2021-09-13 22:55:50 +0700

    Update template for menu fragment
    - Refactor displayMenuPerizinan - use switch, remove redundant html

--- modules/template/menu.php
+++ modules/template/menu.php
@@ -1,69 +1,41 @@
 <?php
 function displayMenuPerizinan(string $role)
 {
-  if ($role == 'Psef.Verifikator') {
+  $route = "";
+
+  switch ($role) {
+    case "Psef.Verifikator":
+      $route = "routing('perizinan_verif')";
+      break;
+    case "Psef.Kasi":
+      $route = "routing('perizinan_kasi')";
+      break;
+    case "Psef.Kasubdit":
+      $route = "routing('perizinan_kasubdit')";
+      break;
+    case "Psef.Diryanfar":
+      $route = "routing('perizinan_diryanfar')";
+      break;
+    case "Psef.Dirjen":
+      $route = "routing('perizinan_dirjen')";
+      break;
+    case "Psef.ValidatorSertifikat":
+      $route = "routing('perizinan_validator')";
+      break;
+    case "Psef.Admin":
+      $route = "routing('perizinan_admin')";
+      break;
+    case "":
+      $route = "routing('perizinan_user')";
+      break;
+  }
 ?>
-    <li id="menu_perizinan_verif" style="display:block" class="sidebar-item">
-      <a onclick="routing('perizinan_verif')" class="sidebar-link two-column has-arrow waves-effect waves-dark" href="javascript:void(0)" aria-expanded="false"><i class="fa fa-id-card"></i><span class="hide-menu">Tanda Terdaftar </span></a>
-    </li>
-  <?php
-  }
-
-  if ($role == 'Psef.Kasi') {
-  ?>
-    <li id="menu_perizinan_kasi" style="display:block" class="sidebar-item">
-      <a onclick="routing('perizinan_kasi')" class="sidebar-link two-column has-arrow waves-effect waves-dark" href="javascript:void(0)" aria-expanded="false"><i class="fa fa-id-card"></i><span class="hide-menu">Tanda Terdaftar </span></a>
-    </li>
-  <?php
-  }
-
-  if ($role == 'Psef.Kasubdit') {
-  ?>
-    <li id="menu_perizinan_kasubdit" style="display:block" class="sidebar-item">
-      <a onclick="routing('perizinan_kasubdit')" class="sidebar-link two-column has-arrow waves-effect waves-dark" href="javascript:void(0)" aria-expanded="false"><i class="fa fa-id-card"></i><span class="hide-menu">Tanda Terdaftar </span></a>
-    </li>
-  <?php
-  }
-
-  if ($role == 'Psef.Diryanfar') {
-  ?>
-    <li id="menu_perizinan_diryanfar" style="display:block" class="sidebar-item">
-      <a onclick="routing('perizinan_diryanfar')" class="sidebar-link two-column has-arrow waves-effect waves-dark" href="javascript:void(0)" aria-expanded="false"><i class="fa fa-id-card"></i><span class="hide-menu">Tanda Terdaftar </span></a>
-    </li>
-  <?php
-  }
-
-  if ($role == 'Psef.Dirjen') {
-  ?>
-    <li id="menu_perizinan_dirjen" style="display:block" class="sidebar-item">
-      <a onclick="routing('perizinan_dirjen')" class="sidebar-link two-column has-arrow waves-effect waves-dark" href="javascript:void(0)" aria-expanded="false"><i class="fa fa-id-card"></i><span class="hide-menu">Tanda Terdaftar </span></a>
-    </li>
-  <?php
-  }
-
-  if ($role == 'Psef.ValidatorSertifikat') {
-  ?>
-    <li id="menu_perizinan_validator" style="display:block" class="sidebar-item">
-      <a onclick="routing('perizinan_validator')" class="sidebar-link two-column has-arrow waves-effect waves-dark" href="javascript:void(0)" aria-expanded="false"><i class="fa fa-id-card"></i><span class="hide-menu">Tanda Terdaftar </span></a>
-    </li>
-  <?php
-  }
-
-  if ($role == 'Psef.Admin') {
-  ?>
-    <li id="menu_perizinan_admin" style="display:block" class="sidebar-item">
-      <a onclick="routing('perizinan_admin')" class="sidebar-link two-column has-arrow waves-effect waves-dark" href="javascript:void(0)" aria-expanded="false"><i class="fa fa-id-card"></i><span class="hide-menu">Tanda Terdaftar </span></a>
-    </li>
-  <?php
-  }
-
-  if ($role == '') {
-  ?>
-    <li id="menu_perizinan_user" style="display:block" class="sidebar-item">
-      <a onclick="routing('perizinan_user')" class="sidebar-link two-column has-arrow waves-effect waves-dark" href="javascript:void(0)" aria-expanded="false"><i class="fa fa-id-card"></i><span class="hide-menu">Tanda Terdaftar </span></a>
-    </li>
-  <?php
-  }
+  <li class="sidebar-item">
+    <a onclick="<?php echo $route; ?>" class="sidebar-link two-column has-arrow" href="javascript:void()" aria-expanded="false">
+      <i class="fa fa-id-card"></i>Tanda Terdaftar
+    </a>
+  </li>
+<?php
 }
 
 function displayMenuTransaksi(string $role)
