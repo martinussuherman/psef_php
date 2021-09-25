0f85535b273cbd36dd918128314216764c1196c0
Martinus Suherman <martinus.suherman@gmail.com>
2021-09-25 11:15:33 +0700

    Update main module
    - Rearrange scripts based on location

--- main.php
+++ main.php
@@ -345,9 +345,74 @@ include('modules/template/menu.php');
     </div>
   </aside>
   <div class="chat-windows"></div>
+
   <!-- ============================================================== -->
   <!-- All Jquery -->
   <!-- ============================================================== -->
+  <script src="assets/libs/jquery/dist/jquery.min.js"></script>
+  <!-- Bootstrap tether Core JavaScript -->
+  <script src="assets/libs/popper.js/dist/umd/popper.min.js"></script>
+  <script src="assets/libs/bootstrap/dist/js/bootstrap.min.js"></script>
+  <!-- Sweet alert -->
+  <script src="assets/libs/sweetalert2/dist/sweetalert2.all.min.js"></script>
+  <!-- Sweet alert -->
+  <script src="assets/libs/select2/dist/js/select2.full.min.js"></script>
+  <!-- Taostr -->
+  <script src="assets/libs/toastr/build/toastr.min.js"></script>
+  <!-- Bootstrap Switch -->
+  <script src="assets/libs/bootstrap-switch/dist/js/bootstrap-switch.min.js"></script>
+  <!-- slimscrollbar scrollbar JavaScript -->
+  <script src="assets/libs/perfect-scrollbar/dist/perfect-scrollbar.jquery.min.js"></script>
+  <script src="assets/extra-libs/sparkline/sparkline.js"></script>
+  <!--Datatables -->
+  <script src="assets/extra-libs/DataTables/datatables.min.js"></script>
+
+  <!-- apps -->
+  <script src="dist/js/app.js"></script>
+  <script src="dist/js/app.init.horizontal-fullwidth.js"></script>
+  <script src="dist/js/app-style-switcher.horizontal.js"></script>
+  <!--Wave Effects -->
+  <script src="dist/js/waves.js"></script>
+  <!--Menu sidebar -->
+  <script src="dist/js/sidebarmenu.js"></script>
+  <!--Custom JavaScript -->
+  <script src="dist/js/custom.js"></script>
+
+  <script src="assets/js/stepbar.js"></script>
+  <script src="assets/js/print.min.js"></script>
+  <script src="assets/js/config.js?5" defer></script>
+  <script src="assets/js/jquery.number.min.js"></script>
+  <script src="assets/js/moment.min.js"></script>
+  <script src="assets/js/keyboardShortcut.js"></script>
+  <script src="assets/js/async.min.js"></script>
+  <script src="assets/js/jquery.form.min.js" defer></script>
+  <script src="assets/js/tinymce.min.js"></script>
+  <script src="assets/js/quill.min.js"></script>
+  <!-- <script src="assets/js/jquery.easyui.min.js"></script> -->
+
+  <!-- start - This is for export functionality only -->
+  <script src="https://cdn.datatables.net/buttons/1.5.1/js/dataTables.buttons.min.js"></script>
+  <script src="https://cdn.datatables.net/buttons/1.5.1/js/buttons.flash.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.32/pdfmake.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.32/vfs_fonts.js"></script>
+  <script src="https://cdn.datatables.net/buttons/1.5.1/js/buttons.html5.min.js"></script>
+  <script src="https://cdn.datatables.net/buttons/1.5.1/js/buttons.print.min.js"></script>
+  <!-- Handlebars -->
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/handlebars.js/2.0.0/handlebars.js"></script>
+
+  <script>
+    //=============================================//
+    //    File export                              //
+    //=============================================//
+    $('#file_export').DataTable({
+      dom: 'Bfrtip',
+      buttons: [
+        'copy', 'csv', 'excel', 'pdf', 'print'
+      ]
+    });
+    $('.buttons-copy, .buttons-csv, .buttons-print, .buttons-pdf, .buttons-excel').addClass('btn btn-cyan text-white mr-1');
+  </script>
   <script>
     function setCookie(cname, cvalue, exdays) {
       var d = new Date();
@@ -399,67 +464,7 @@ include('modules/template/menu.php');
       window.location = 'https://psef.kemkes.go.id/logout.php';
     }
   </script>
-  <script src="assets/libs/jquery/dist/jquery.min.js"></script>
-  <!-- Bootstrap tether Core JavaScript -->
-  <script src="assets/libs/popper.js/dist/umd/popper.min.js"></script>
-  <script src="assets/libs/bootstrap/dist/js/bootstrap.min.js"></script>
-  <!-- apps -->
-  <script src="dist/js/app.js"></script>
-  <script src="dist/js/app.init.horizontal-fullwidth.js"></script>
-  <script src="dist/js/app-style-switcher.horizontal.js"></script>
-  <!-- slimscrollbar scrollbar JavaScript -->
-  <script src="assets/libs/perfect-scrollbar/dist/perfect-scrollbar.jquery.min.js"></script>
-  <script src="assets/extra-libs/sparkline/sparkline.js"></script>
-  <!--Wave Effects -->
-  <script src="dist/js/waves.js"></script>
-  <!--Menu sidebar -->
-  <script src="dist/js/sidebarmenu.js"></script>
-  <!--Custom JavaScript -->
-  <script src="dist/js/custom.js"></script>
-  <script src="assets/js/stepbar.js"></script>
-  <!--Datatables -->
-  <script src="assets/extra-libs/DataTables/datatables.min.js"></script>
-  <!-- start - This is for export functionality only -->
-  <script src="https://cdn.datatables.net/buttons/1.5.1/js/dataTables.buttons.min.js"></script>
-  <script src="https://cdn.datatables.net/buttons/1.5.1/js/buttons.flash.min.js"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.32/pdfmake.min.js"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.32/vfs_fonts.js"></script>
-  <script src="https://cdn.datatables.net/buttons/1.5.1/js/buttons.html5.min.js"></script>
-  <script src="https://cdn.datatables.net/buttons/1.5.1/js/buttons.print.min.js"></script>
-  <script>
-    //=============================================//
-    //    File export                              //
-    //=============================================//
-    $('#file_export').DataTable({
-      dom: 'Bfrtip',
-      buttons: [
-        'copy', 'csv', 'excel', 'pdf', 'print'
-      ]
-    });
-    $('.buttons-copy, .buttons-csv, .buttons-print, .buttons-pdf, .buttons-excel').addClass('btn btn-cyan text-white mr-1');
-  </script>
 
-  <!-- Handlebars -->
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/handlebars.js/2.0.0/handlebars.js"></script>
-  <!-- Sweet alert -->
-  <script src="assets/libs/sweetalert2/dist/sweetalert2.all.min.js"></script>
-  <!-- Sweet alert -->
-  <script src="assets/libs/select2/dist/js/select2.full.min.js"></script>
-  <!-- Taostr -->
-  <script src="assets/libs/toastr/build/toastr.min.js"></script>
-  <!-- Bootstrap Switch -->
-  <script src="assets/libs/bootstrap-switch/dist/js/bootstrap-switch.min.js"></script>
-  <script src="assets/js/print.min.js"></script>
-  <script src="assets/js/config.js?5" defer></script>
-  <script src="assets/js/jquery.number.min.js"></script>
-  <script src="assets/js/moment.min.js"></script>
-  <script src="assets/js/keyboardShortcut.js"></script>
-  <script src="assets/js/async.min.js"></script>
-  <script src="assets/js/jquery.form.min.js" defer></script>
-  <script src="assets/js/tinymce.min.js"></script>
-  <script src="assets/js/quill.min.js"></script>
-  <!-- <script src="assets/js/jquery.easyui.min.js"></script> -->
   <script type="text/javascript">
     const sid = "<?php echo $_COOKIE['sid'] ?>";
     const role = "<?php echo $_COOKIE['role'] ?>";
