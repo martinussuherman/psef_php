44c91af48b2e2226a828909a04e6cbc711ff5198
Martinus Suherman <martinus.suherman@gmail.com>
2021-10-11 17:14:54 +0700

    Update home page
    - [WIP] Refactor: use function for html header display

--- cek_login.php
+++ cek_login.php
@@ -3,41 +3,10 @@
 
 <?php
 include('analytics.php');
+include('modules/template/header.php');
+
+displayHeader();
 ?>
-
-<head>
-  <meta charset="utf-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <!-- Tell the browser to be responsive to screen width -->
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <meta name="description" content="">
-  <meta name="author" content="">
-  <!-- Favicon icon -->
-  <link rel="icon" type="image/png" sizes="16x16" href="/assets/internal/logo.png">
-  <title>Selamat Datang di Sistem PSEF</title>
-
-  <!-- Global site tag (gtag.js) - Google Analytics -->
-  <script async src="https://www.googletagmanager.com/gtag/js?id=UA-205037947-1"></script>
-  <script>
-    window.dataLayer = window.dataLayer || [];
-    function gtag(){dataLayer.push(arguments);}
-    gtag('js', new Date());
-
-    gtag('config', 'UA-205037947-1');
-  </script>
-
-  <!-- Custom CSS -->
-  <link href="dist/css/style.min.css" rel="stylesheet">
-  <!-- Toastr -->
-  <link href="../../assets/libs/toastr/build/toastr.min.css" rel="stylesheet">
-  <link href="dist/css/quill.snow.css" rel="stylesheet">
-  <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-  <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-  <!--[if lt IE 9]>
-    <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-    <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-<![endif]-->
-</head>
 
 <body>
   <div class="main-wrapper" style="background: #eef5f9;">
