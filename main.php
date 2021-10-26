8f42bd94f197d3471f9debf0ce2ca2d5606fe2cf
Martinus Suherman <martinus.suherman@gmail.com>
2021-10-26 19:52:03 +0700

    Update main module
    - Refactor: use function to load scripts

--- main.php
+++ main.php
@@ -8,8 +8,7 @@ $role = $_SESSION["role"];
 <html lang="en" dir="ltr">
 
 <?php
-include('modules/template/menu.php');
-include('modules/template/header.php');
+include("modules/template/pageScripts.php");
 include("configReader.php");
 
 $settingData = readConfig();
@@ -198,53 +197,7 @@ displayHeader();
   </aside>
   <div class="chat-windows"></div>
 
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js" integrity="sha512-+NqPlbbtM1QqiK8ZAo4Yrj2c4lNQoGv8P79DPtKzj++l5jnN39rHA/xsqn8zE9l0uSoxaCdrOgFs6yjyfbBxSg==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.0/js/bootstrap.min.js" integrity="sha512-pgGHFWjBtbKHTTDW5buGZ9mU0nGfxNavf5kWK/Od2ugA//9FuMHAunkAiMe5jeL/5WW1r0UxwKi6D5LpMOJD3w==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-switch/3.3.4/js/bootstrap-switch.min.js" integrity="sha512-J+763o/bd3r9iW+gFEqTaeyi+uAphmzkE/zU8FxY6iAvD3nQKXa+ZAWkBI9QS9QkYEKddQoiy0I5GDxKf/ORBA==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.29.1/moment.min.js" integrity="sha512-qTXRIMyZIFb8iQcfjXWCO8+M5Tbc38Qi5WzdPOYZHIlZpzBHG3L3by84BBBOiRGiEb7KKtAOAs5qYdUiZiQNNQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/quill/1.3.7/quill.min.js" integrity="sha512-P2W2rr8ikUPfa31PLBo5bcBQrsa+TNj8jiKadtaIrHQGMo6hQM6RdPjQYxlNguwHz8AwSQ28VkBK6kHBLgd/8g==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/tinymce/4.7.13/tinymce.min.js" integrity="sha512-14mUAc7bPPk/ppYTQKsuQ9hLwjXh/d6mX2y7QKQ3MtjddJysxmIzGcnzR53PJ2/0tvFT5IUfkQqh7QeLd5iH9g==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/2.10.2/umd/popper.min.js" integrity="sha512-nnzkI2u2Dy6HMnzMIkh7CPd1KX445z38XIu4jG1jGw7x5tSL3VBjE44dY4ihMU1ijAQV930SPM12cCFrB18sVw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-
-  <script src="assets/libs/sweetalert2/dist/sweetalert2.all.min.js"></script>
-  <script src="assets/libs/select2/dist/js/select2.full.min.js"></script>
-  <script src="assets/libs/toastr/build/toastr.min.js"></script>
-  <script src="assets/extra-libs/DataTables/datatables.min.js"></script>
-
-  <!-- slimscrollbar scrollbar JavaScript -->
-  <script src="assets/libs/perfect-scrollbar/dist/perfect-scrollbar.jquery.min.js"></script>
-  <script src="assets/extra-libs/sparkline/sparkline.js"></script>
-
-  <!-- apps -->
-  <script src="dist/js/app.js"></script>
-  <script src="dist/js/app.init.horizontal-fullwidth.js"></script>
-  <script src="dist/js/app-style-switcher.horizontal.js"></script>
-  <!--Wave Effects -->
-  <script src="dist/js/waves.js"></script>
-  <!--Menu sidebar -->
-  <script src="dist/js/sidebarmenu.js"></script>
-  <!--Custom JavaScript -->
-  <script src="dist/js/custom.js"></script>
-
-  <script src="assets/js/stepbar.js"></script>
-  <script src="assets/js/print.min.js"></script>
-  <script src="assets/js/config.js?5" defer></script>
-  <script src="assets/js/jquery.number.min.js"></script>
-  <script src="assets/js/keyboardShortcut.js"></script>
-  <script src="assets/js/async.min.js"></script>
-  <script src="assets/js/jquery.form.min.js" defer></script>
-  <!-- <script src="assets/js/jquery.easyui.min.js"></script> -->
-
-  <!-- start - This is for export functionality only -->
-  <script src="https://cdn.datatables.net/buttons/1.5.1/js/dataTables.buttons.min.js"></script>
-  <script src="https://cdn.datatables.net/buttons/1.5.1/js/buttons.flash.min.js"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.32/pdfmake.min.js"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.32/vfs_fonts.js"></script>
-  <script src="https://cdn.datatables.net/buttons/1.5.1/js/buttons.html5.min.js"></script>
-  <script src="https://cdn.datatables.net/buttons/1.5.1/js/buttons.print.min.js"></script>
-  <!-- Handlebars -->
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/handlebars.js/2.0.0/handlebars.js"></script>
+  <?php displayPageScripts(); ?>
 
   <script>
     //=============================================//
