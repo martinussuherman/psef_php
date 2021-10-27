ca50a04ee516e54ecf7eeb3367b3207feb60bd5a
Martinus Suherman <martinus.suherman@gmail.com>
2021-10-27 18:15:53 +0700

    Create api calling module

---
+++ apiCall.php
@@ -0,0 +1,49 @@
+<?php
+function initCurl(string $apiUrl, string $token)
+{
+  $curl = curl_init();
+  curl_setopt_array($curl, array(
+    CURLOPT_URL => $apiUrl,
+    CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_TIMEOUT => 30,
+    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_2TLS,
+    CURLOPT_HTTPHEADER => array(
+      "cache-control: no-cache",
+      'Content-Type: application/json',
+      'Authorization: Bearer ' . $token,
+    ),
+  ));
+
+  return $curl;
+}
+
+function execCurl(CurlHandle|false $curl)
+{
+  if ($curl === false) {
+    return false;
+  }
+
+  $response = curl_exec($curl);
+  $errorMsg = curl_error($curl);
+  curl_close($curl);
+
+  if ($errorMsg != "") {
+    return $errorMsg;
+  }
+
+  return json_decode($response, false);
+}
+
+function callGetApi(string $apiUrl, string $token)
+{
+  $curl = initCurl($apiUrl,  $token);
+  return execCurl($curl);
+}
+
+function callPostApi(string $apiUrl, string $token, $postData)
+{
+  $curl = initCurl($apiUrl,  $token);
+  curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+  curl_setopt($curl, CURLOPT_POSTFIELDS, $postData);
+  return execCurl($curl);
+}
