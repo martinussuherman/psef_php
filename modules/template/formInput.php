492b0c040e1bf4ee85638f7b39dd263f562af63d
Martinus Suherman <martinus.suherman@gmail.com>
2021-10-28 19:00:17 +0700

    Update template for form input fragment
    - Add function displayFileInputRow

--- modules/template/formInput.php
+++ modules/template/formInput.php
@@ -42,3 +42,72 @@ function displayTextInputRow(
   </div>
 <?php
 }
+
+function displayFileInputRow(
+  string $label,
+  string $fileUrl,
+  string $fileName,
+  string $inputName,
+  string $closeElementId,
+  string $inputElementId,
+  string $hiddenInputElementId,
+  string $checkValue,
+  string $checkName,
+  string $reasonValue,
+  string $reasonName,
+  string $reasonPlaceholder,
+  bool $inputEnabled,
+  bool $checkEnabled
+) {
+  $inputState = $inputEnabled ? "" : "disabled";
+  $inputRequired = $inputEnabled ? "required" : "";
+  $checkState = $checkEnabled ? "" : "disabled";
+  $checkRequired = $checkEnabled ? "required" : "";
+?>
+  <div class="row">
+    <div class="col-md-5">
+      <div class="form-group">
+        <label>
+          <?php echo $label; ?>
+        </label>
+
+        <div>
+          <a class="btn btn-block btn-outline-secondary text-left" href="<?php echo $fileUrl; ?>" target="_blank" id="<?php echo $closeElementId; ?>">
+            <?php echo $fileName; ?>
+          </a>
+        </div>
+
+        <?php
+        if ($inputEnabled) {
+        ?>
+          <input type="file" class="form-control" id="<?php echo $inputElementId; ?>" />
+
+          <small class="form-text text-muted">
+            *Berkas yang anda upload wajib PDF & size file maksimal 5 MB
+          </small>
+
+          <input type="hidden" name="<?php echo $inputName; ?>" id="<?php echo $hiddenInputElementId; ?>" />
+        <?php
+        }
+        ?>
+      </div>
+    </div>
+
+    <div class="col-md-2">
+      <div class="form-group text-center">
+        <label class="control-label">Sesuai</label>
+        <input type="checkbox" class="form-control" name="<?php echo $checkName; ?>" value="<?php echo $checkValue; ?>" <?php echo $checkState; ?> />
+      </div>
+    </div>
+
+    <div class="col-md-5">
+      <div class="form-group">
+        <label class="control-label">Keterangan</label>
+        <textarea class="form-control" name="<?php echo $reasonName; ?>" placeholder="<?php echo $reasonPlaceholder; ?>" <?php echo $checkRequired; ?> <?php echo $checkState; ?>><?php echo $reasonValue; ?></textarea>
+      </div>
+    </div>
+  </div>
+
+<?php
+}
+
