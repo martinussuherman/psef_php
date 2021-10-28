55c45215f4a9e4bd1772adc87b9c18c1fa230011
Martinus Suherman <martinus.suherman@gmail.com>
2021-10-29 01:13:41 +0700

    Update index.php
    - Use single quote for route mapping, because double quote throw error for parameterized route

--- index.php
+++ index.php
@@ -3,6 +3,7 @@ require_once("{$_SERVER['DOCUMENT_ROOT']}/router.php");
 
 $homeRoute = empty($_COOKIE["sid"]) ? "cek_login.php" : "main.php";
 
-get("/", $homeRoute);
-get("/login", "login.php");
-get("/logout", "logout.php");
+get('/', $homeRoute);
+get('/login', 'login.php');
+get('/logout', 'logout.php');
+get('/view-nib/$nib', 'modules/nib/displayNib.php');
