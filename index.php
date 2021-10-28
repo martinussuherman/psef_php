37fb4562309846f0e94785d5bc6a747a04acd5ed
Martinus Suherman <martinus.suherman@gmail.com>
2021-10-28 22:33:53 +0700

    Update index.php - use routing

--- index.php
+++ index.php
@@ -1,6 +1,8 @@
 <?php
-    if(empty($_COOKIE['sid'])){
-        include('cek_login.php');
-    }else{
-        include('main.php');
-    }
+require_once("{$_SERVER['DOCUMENT_ROOT']}/router.php");
+
+$homeRoute = empty($_COOKIE["sid"]) ? "cek_login.php" : "main.php";
+
+get("/", $homeRoute);
+get("/login", "login.php");
+get("/logout", "logout.php");
