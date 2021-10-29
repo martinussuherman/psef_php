05fd90db03baddfa625da9423bbc1998a83d1a38
Martinus Suherman <martinus.suherman@gmail.com>
2021-10-29 11:09:59 +0700

    Update index.php
    - Change route from cek_login.php to home.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 require_once("{$_SERVER['DOCUMENT_ROOT']}/router.php");
 
-$homeRoute = empty($_COOKIE["sid"]) ? "cek_login.php" : "main.php";
+$homeRoute = empty($_COOKIE["sid"]) ? "home.php" : "main.php";
 
 get('/', $homeRoute);
 get('/login', 'login.php');
