038f42d3417954ae23c897ddbfaa823607608f93
Martinus Suherman <martinus.suherman@gmail.com>
2021-10-30 22:23:11 +0700

    Update index.php
    - Change login detection from using cookie to session variable

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 require_once("{$_SERVER['DOCUMENT_ROOT']}/router.php");
 
-$homeRoute = empty($_COOKIE["sid"]) ? "home.php" : "main.php";
+$homeRoute = empty($_SESSION["accessToken"]) ? "home.php" : "main.php";
 
 get('/', $homeRoute);
 get('/login', 'login.php');
