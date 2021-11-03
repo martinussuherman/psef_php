2dcdf7d040f1a31632a47fffe857dfc6f7003db5
Martinus Suherman <martinus.suherman@gmail.com>
2021-11-03 17:13:50 +0700

    Update oss sso receive token module
    - Add get parameter for kode izin

--- modules/sso/receiveToken.php
+++ modules/sso/receiveToken.php
@@ -4,7 +4,8 @@ require_once("apiCall.php");
 
 $ssoAccessToken = $_GET["access-token"];
 $ssoRefreshToken = $_GET["refresh_token"];
+$ssoKodeIzin = $_GET["kd_izin"];
 $from = $_SERVER["REMOTE_ADDR"];
 
-file_put_contents("oss-sso-receive-token.log", "from: {$from} access token: {$ssoAccessToken} - refresh token: {$ssoRefreshToken}\n", FILE_APPEND);
+file_put_contents("oss-sso-receive-token.log", "from: {$from} access token: {$ssoAccessToken} - refresh token: {$ssoRefreshToken} - kode izin: {$ssoKodeIzin}\n", FILE_APPEND);
 
