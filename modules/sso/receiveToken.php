5d0aec0351ad0263288ab9836408b4bf75a6f3e9
Martinus Suherman <martinus.suherman@gmail.com>
2021-11-03 17:11:34 +0700

    Update oss sso receive token module
    - Change get parameter from refresh-token to refresh_token

--- modules/sso/receiveToken.php
+++ modules/sso/receiveToken.php
@@ -3,7 +3,7 @@ require_once("configReader.php");
 require_once("apiCall.php");
 
 $ssoAccessToken = $_GET["access-token"];
-$ssoRefreshToken = $_GET["refresh-token"];
+$ssoRefreshToken = $_GET["refresh_token"];
 $from = $_SERVER["REMOTE_ADDR"];
 
 file_put_contents("oss-sso-receive-token.log", "from: {$from} access token: {$ssoAccessToken} - refresh token: {$ssoRefreshToken}\n", FILE_APPEND);
