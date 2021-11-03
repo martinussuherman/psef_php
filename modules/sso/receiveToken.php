51c6347d0f7dd26049b22b2b76daf818051842bd
Martinus Suherman <martinus.suherman@gmail.com>
2021-11-03 15:35:18 +0700

    Update oss sso receive token module - log to file

--- modules/sso/receiveToken.php
+++ modules/sso/receiveToken.php
@@ -1,3 +1,9 @@
 <?php
+require_once("configReader.php");
+require_once("apiCall.php");
+
 $ssoAccessToken = $_GET["access-token"];
 $ssoRefreshToken = $_GET["refresh-token"];
+$from = $_SERVER["REMOTE_ADDR"];
+
+file_put_contents("oss-sso-receive-token.log", "from: {$from} access token: {$ssoAccessToken} - refresh token: {$ssoRefreshToken}");
