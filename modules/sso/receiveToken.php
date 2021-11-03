1b6e3d7cd9ea767fc692c3056f523abe45abb4a8
Martinus Suherman <martinus.suherman@gmail.com>
2021-11-03 15:44:18 +0700

    Update oss sso receive token module
    - Update log to append instead of overwrite

--- modules/sso/receiveToken.php
+++ modules/sso/receiveToken.php
@@ -6,4 +6,5 @@ $ssoAccessToken = $_GET["access-token"];
 $ssoRefreshToken = $_GET["refresh-token"];
 $from = $_SERVER["REMOTE_ADDR"];
 
-file_put_contents("oss-sso-receive-token.log", "from: {$from} access token: {$ssoAccessToken} - refresh token: {$ssoRefreshToken}");
+file_put_contents("oss-sso-receive-token.log", "from: {$from} access token: {$ssoAccessToken} - refresh token: {$ssoRefreshToken}", FILE_APPEND);
+
