33c27d591e9fa07ee37cc8aa5d9374de696b2d34
Martinus Suherman <martinus.suherman@gmail.com>
2021-11-03 15:55:11 +0700

    Update oss sso receive token module
    - Update log to add newline per entry

--- modules/sso/receiveToken.php
+++ modules/sso/receiveToken.php
@@ -6,5 +6,5 @@ $ssoAccessToken = $_GET["access-token"];
 $ssoRefreshToken = $_GET["refresh-token"];
 $from = $_SERVER["REMOTE_ADDR"];
 
-file_put_contents("oss-sso-receive-token.log", "from: {$from} access token: {$ssoAccessToken} - refresh token: {$ssoRefreshToken}", FILE_APPEND);
+file_put_contents("oss-sso-receive-token.log", "from: {$from} access token: {$ssoAccessToken} - refresh token: {$ssoRefreshToken}\n", FILE_APPEND);
 
