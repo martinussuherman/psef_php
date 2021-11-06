560a5289de1aa9e019258343f526baf579e85a50
Martinus Suherman <martinus.suherman@gmail.com>
2021-11-06 10:50:12 +0700

    Update oss sso receive token module
    - Capture the full query string

--- modules/sso/receiveToken.php
+++ modules/sso/receiveToken.php
@@ -6,6 +6,8 @@ $ssoAccessToken = $_GET["access-token"];
 $ssoRefreshToken = $_GET["refresh_token"];
 $ssoKodeIzin = $_GET["kd_izin"];
 $from = $_SERVER["REMOTE_ADDR"];
+$query = $_SERVER["QUERY_STRING"];
 
+file_put_contents("oss-sso-receive-token.log", "Query string: {$query}\n", FILE_APPEND);
 file_put_contents("oss-sso-receive-token.log", "from: {$from} access token: {$ssoAccessToken} - refresh token: {$ssoRefreshToken} - kode izin: {$ssoKodeIzin}\n", FILE_APPEND);
 
