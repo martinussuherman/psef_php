53dfb414d251696aaa16ea9ce81a2ba1abbc7091
Martinus Suherman <martinus.suherman@gmail.com>
2021-11-17 17:34:03 +0700

    Update index.php
    - Add call-api post endpoint

--- index.php
+++ index.php
@@ -9,3 +9,4 @@ get('/login', 'login.php');
 get('/logout', 'logout.php');
 get('/view-nib/$nib', 'modules/nib/displayNib.php');
 get('/sso/receive-token', 'modules/sso/receiveToken.php');
+post('/call-api', 'api/api.php');
