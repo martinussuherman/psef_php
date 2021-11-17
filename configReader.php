7fd3c5d4e887f66a9bf34124988b32570c5bee5f
Martinus Suherman <martinus.suherman@gmail.com>
2021-11-17 15:38:33 +0700

    Update config reader logic
    - Use abolute path for json file

--- configReader.php
+++ configReader.php
@@ -1,6 +1,6 @@
 <?php
 function readConfig()
 {
-  $fileContent = file_get_contents("appsettings.json");
+  $fileContent = file_get_contents($_SERVER["DOCUMENT_ROOT"] . "/appsettings.json");
   return json_decode($fileContent, false);
 }
