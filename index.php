7d8461d989c50825d897ea29a4e699e8142175aa
Martinus Suherman <martinus.suherman@gmail.com>
2021-11-26 12:26:40 +0700

    Update index.php
    - Add IdentityServer callback endpoint

--- index.php
+++ index.php
@@ -9,4 +9,5 @@ get('/login', 'login.php');
 get('/logout', 'logout.php');
 get('/view-nib/$nib', 'modules/nib/displayNib.php');
 get('/sso/receive-token', 'modules/sso/receiveToken.php');
+get('/sso/ids-sso', 'modules/sso/identityServerCallback.php');
 post('/call-api', 'api/api.php');
