bd9bff6b2a3f43e66628d2529fceea5d6d0cd8a3
Martinus Suherman <martinus.suherman@gmail.com>
2021-11-26 12:19:25 +0700

    Update oss sso receive token module
    - Redirect to IdentityServer, passing parameters

--- modules/sso/receiveToken.php
+++ modules/sso/receiveToken.php
@@ -6,9 +6,20 @@ $ssoAccessToken = $_GET["access-token"];
 $ssoRefreshToken = $_GET["refresh_token"];
 $ssoKodeIzin = $_GET["kd_izin"];
 $ssoIdIzin = $_GET["id_izin"];
+$redirectTo = $_SERVER["SERVER_NAME"];
 $from = $_SERVER["REMOTE_ADDR"];
 $query = $_SERVER["QUERY_STRING"];
 
 file_put_contents("oss-sso-receive-token.log", "Query string: {$query}\n", FILE_APPEND);
 file_put_contents("oss-sso-receive-token.log", "from: {$from} access token: {$ssoAccessToken} - refresh token: {$ssoRefreshToken} - kode izin: {$ssoKodeIzin} - id izin: {$ssoIdIzin}\n", FILE_APPEND);
 
+$settingData = readConfig();
+$location =
+  "Location: {$settingData->identity->identityServerUrl}/Oss" .
+  "?accessToken={$ssoAccessToken}" .
+  "&refreshToken={$ssoRefreshToken}" .
+  "&kodeIzin={$ssoKodeIzin}" .
+  "&idIzin={$ssoIdIzin}" .
+  "&redirectTo={$redirectTo}";
+
+header($location);
