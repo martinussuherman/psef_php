1ddc4f4dd8bbb647356ea2e6263e170b129ae7bb
Martinus Suherman <martinus.suherman@gmail.com>
2021-11-29 12:10:16 +0700

    Update homepage
    - Add psef js asset

--- home.php
+++ home.php
@@ -221,6 +221,7 @@ displayHeader();
   <script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/2.1.4/toastr.min.js" integrity="sha512-lbwH47l/tPXJYG9AcFNoJaTMhGvYWhVM9YI43CT+uteTRRaiLCui8snIgyAN8XWgNjNhCqlAUdzZptso6OCoFQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 
   <script src="/assets/js/config.js"></script>
+  <script src="/modules/scripts/psef.js"></script>
 
   <script>
     $('[data-toggle="tooltip"]').tooltip();
