cac8e13bba708e6b758fd9b7f4a9e5ba16f5fe0a
Martinus Suherman <martinus.suherman@gmail.com>
2021-11-29 12:06:15 +0700

    Update oss sso receive token module
    - Save sso get parameter to session variable

--- modules/sso/receiveToken.php
+++ modules/sso/receiveToken.php
@@ -10,6 +10,11 @@ $redirectTo = $_SERVER["SERVER_NAME"];
 $from = $_SERVER["REMOTE_ADDR"];
 $query = $_SERVER["QUERY_STRING"];
 
+$_SESSION["ssoAccessToken"] = $ssoAccessToken;
+$_SESSION["ssoRefreshToken"] = $ssoRefreshToken;
+$_SESSION["ssoKodeIzin"] = $ssoKodeIzin;
+$_SESSION["ssoIdIzin"] = $ssoIdIzin;
+
 file_put_contents("oss-sso-receive-token.log", "Query string: {$query}\n", FILE_APPEND);
 file_put_contents("oss-sso-receive-token.log", "from: {$from} access token: {$ssoAccessToken} - refresh token: {$ssoRefreshToken} - kode izin: {$ssoKodeIzin} - id izin: {$ssoIdIzin}\n", FILE_APPEND);
 
