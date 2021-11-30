04385577f9c6e4e4bdf4cff7b9a34c50487ed4ce
Martinus Suherman <martinus.suherman@gmail.com>
2021-11-30 11:46:56 +0700

    Update index.php
    - Add dashboard route
    - No more set / route based on session variable

--- index.php
+++ index.php
@@ -2,9 +2,8 @@
 set_include_path(get_include_path() . PATH_SEPARATOR . $_SERVER["DOCUMENT_ROOT"]);
 require_once("router.php");
 
-$homeRoute = empty($_SESSION["accessToken"]) ? "home.php" : "main.php";
-
-get('/', $homeRoute);
+get('/', 'home.php');
+get('/dashboard', 'main.php');
 get('/login', 'login.php');
 get('/logout', 'logout.php');
 get('/view-nib/$nib', 'modules/nib/displayNib.php');
