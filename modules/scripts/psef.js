230b755e780e3289d8fb23b631a3fc8aa65eab5d
Martinus Suherman <martinus.suherman@gmail.com>
2021-12-10 17:26:50 +0700

    Update psef.js

--- modules/scripts/psef.js
+++ modules/scripts/psef.js
@@ -25,7 +25,25 @@ function setNumberOnlyInputFilter(textbox) {
         return /^\d*$/.test(value); // Allow digits only, using a RegExp
     });
 }
-function displayOssSsoToastr(statusCode, message) {
+function setPhoneNumberInputFilter(textbox) {
+    setInputFilter(textbox, function (value) {
+      return /^[+\d\s-+]*$/.test(value);
+    });
+}
+function setSaveButtonStateOnInputChanged(formElementId, saveButtonElementId) {
+    $(formElementId)
+        .each(function () {
+        $(this).data("serialized", $(this).serialize());
+    })
+        .on("change input", function () {
+        $(this)
+            .find(saveButtonElementId)
+            .prop("disabled", $(this).serialize() == $(this).data("serialized"));
+    })
+        .find(saveButtonElementId)
+        .prop("disabled", true);
+}
+function setToastrOptions() {
     var options = {
         closeButton: true,
         debug: false,
@@ -42,6 +60,10 @@ function displayOssSsoToastr(statusCode, message) {
         showMethod: "fadeIn",
         hideMethod: "fadeOut"
     };
+    return options;
+}
+function displayOssSsoToastr(statusCode, message) {
+    var options = setToastrOptions();
     var title = "OSS SSO";
     if (statusCode == 200) {
         toastr.success(message + "<br /> Silahkan klik Masuk untuk melanjutkan ke dalam dasboard", title, options);
@@ -55,4 +77,57 @@ function displayHomeNewsItem(resourceUrl, news, index) {
     quill.setContents(JSON.parse(news.content));
     quill.disable();
     $('.ql-editor').css('padding', '0');
+}
+function loadData(url, token) {
+    return $.ajax({
+        url: url,
+        method: "GET",
+        beforeSend: function (xhr) {
+            xhr.setRequestHeader("Authorization", "Bearer " + token);
+        },
+        dataType: "json"
+    });
+}
+function loadAndDisplayNib(nib, apiServerUrl, token, inputElementId, statusElementId, viewElementId) {
+    if (nib == undefined || nib == "") {
+        return;
+    }
+    loadData(apiServerUrl + "/api/v0.1/OssInfo/OssFullInfo?id=" + nib, token).then(function (data) {
+        if (data.keterangan == 'Data NIB tidak ditemukan' ||
+            data.keterangan == 'NIB harus 13 karakter.' ||
+            data.keterangan == 'Api Key tidak valid') {
+            $(viewElementId).html("");
+            $(statusElementId).css('color', 'red');
+            $(statusElementId).html('Data NIB Tidak di Temukan');
+            $(inputElementId).removeClass('is-valid').addClass('is-invalid');
+            return;
+        }
+        $(statusElementId).css('color', 'green');
+        $(statusElementId).html("\n      Data NIB Dapat di Gunakan<br>\n      <a href=\"/view-nib/" + nib + "\" class=\"btn btn-primary\" target=\"_blank\">\n        Periksa Detail NIB\n      </a>");
+        $(inputElementId).removeClass('is-invalid').addClass('is-valid');
+        $(viewElementId).html("\n      <table class=\"table table-bordered\">\n        <thead class=\"thead-light\">\n          <tr>\n            <th scope=\"col\">Nama Perusahaan</th>\n            <th scope=\"col\">NIB</th>\n            <th scope=\"col\">NPWP Perusahaan</th>\n            <th scope=\"col\">Nomor Telepon Perusahaan</th>\n            <th scope=\"col\">Alamat Perusahaan</th>\n          </tr>\n        </thead>\n        <tbody>\n          <tr>\n            <th>" + data.namaPerseroan + "</th>\n            <th>" + data.nib + "</th>\n            <th>" + data.npwpPerseroan + "</th>\n            <th>" + data.nomorTelponPerseroan + "</th>\n            <th>" + data.alamatPerseroan + "</th>\n          </tr>\n        </tbody>\n      </table>");
+    });
+}
+function dataTablePemohon(elementSelector, url) {
+    jQuery(function () {
+        $(elementSelector).DataTable({
+            processing: true,
+            serverSide: true,
+            ajax: {
+                url: url,
+                method: "POST"
+            },
+            columns: [
+                { data: "id" },
+                { data: "phone" },
+                { data: "address" },
+                { data: "nib" },
+                { data: "companyName" },
+                { data: "penanggungJawab" }
+                // "userId": "string",
+                // "name": "string",
+                // "email": "string"
+            ]
+        });
+    });
 }
