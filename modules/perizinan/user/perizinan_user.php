ad8391c289b19c337f6dc5741545586a2ce880a3
Martinus Suherman <martinus.suherman@gmail.com>
2022-02-07 12:03:08 +0700

    Update perizinan_user.php
    - Add session_start
    - Read api url from setting
    - Update loadDataTablePerizinan call

--- modules/perizinan/user/perizinan_user.php
+++ modules/perizinan/user/perizinan_user.php
@@ -1,16 +1,24 @@
 <?php
+session_start();
 set_include_path(get_include_path() . PATH_SEPARATOR . $_SERVER["DOCUMENT_ROOT"]);
 require_once("configReader.php");
 require_once('../template/template.php');
 $settingData = readConfig();
 $fileUrl = $settingData->resourceUrl;
+$apiUrl = $settingData->apiServerUrl;
 ?>
 
 <script>
     var accesstoken = <?php echo json_encode($_COOKIE['accesstoken']); ?>;
 
     $(document).ready(function() {
-      loadDataTablePerizinan(url_api_php, "<?php echo $fileUrl; ?>", "#zero_config");
+      loadDataTablePerizinan(
+        url_api_php,
+        "<?php echo $apiUrl; ?>",
+        "<?php echo $fileUrl; ?>",
+        accesstoken,
+        "#zero_config",
+        ".preloader");
     });
 
     function viewRouting() {
