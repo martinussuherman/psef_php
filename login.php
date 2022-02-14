f937c89cc6fb5c763563036ff9e2ce7e01f518ca
Martinus Suherman <martinus.suherman@gmail.com>
2022-02-14 11:27:45 +0700

    Update login.php - set role to empty string on null

--- login.php
+++ login.php
@@ -25,11 +25,11 @@ $_SESSION["idToken"] = $idToken;
 $_SESSION["accessToken"] = $accessToken;
 $_SESSION["refreshToken"] = $oidc->getRefreshToken();
 $_SESSION["tokenExpired"] = $userClaims->exp;
-$_SESSION["role"] = $userClaims->role;
+$_SESSION["role"] = $userClaims->role ?? "";
 $_SESSION["email"] = $idTokenData->email;
 
 setcookie('sid', $userClaims->sub, time() + 86400, "/");
-setcookie('role', $userClaims->role, time() + 86400, "/");
+setcookie('role', $userClaims->role ?? "", time() + 86400, "/");
 setcookie('email',  $idTokenData->email, time() + 86400, "/");
 setcookie('idtoken', $idToken, time() + 86400, "/");
 setcookie('accesstoken', $accessToken, time() + 86400, "/");
